96ff9ca3a5beb966db6c1f7904e510372eb7c496
salvadorcnlsilva <128598290+salva1211164@users.noreply.github.com>
2025-05-30 16:28:02 +0100

    Created Mission and Point class

--- sprint3/src/main/java/org/lprog/domain/mission/Mission.java
+++ sprint3/src/main/java/org/lprog/domain/mission/Mission.java
@@ -1,4 +1,25 @@
 package org.lprog.domain.mission;
 
+import java.util.Date;
+import java.util.List;
+
 public class Mission {
+
+    public Date startDate;
+    public String Model;
+    public Point StartingPoint;
+    public List<Point> deliveries;
+
+    public Mission(Date startDate, String model, Point startingPoint, List<Point> deliveries) {
+        this.startDate = startDate;
+        this.Model = model;
+        this.StartingPoint = startingPoint;
+        this.deliveries = deliveries;
+    }
+
+    public void addPoint (Point point) {
+        deliveries.add(point);
+        System.out.println("Point " +
+                point.toString() + "added.");
+    }
 }
